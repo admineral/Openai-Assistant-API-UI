5665fd3b209833bd65bb6ce10d23f23f94ce67fd
admineral <50579369+admineral@users.noreply.github.com>
2023-11-23 21:39:29 +0100

    Maintain state of name and instructions when switching tabs and add conditional rendering for file upload status. Experimental Webcrawler-alpha0

--- app/GPT_Builder_components/Left_Side/Configure/ParentComponent.tsx
+++ app/GPT_Builder_components/Left_Side/Configure/ParentComponent.tsx
@@ -2,23 +2,29 @@
 import React, { useState } from 'react';
 import Capabilities from './Skills/Capabilities';
 import UploadFiles_Configure from './Upload_Left/UploadFiles_2';
-import CrawlerInput from './Skills/Crawler/CrawlerInput'; // Add this line
+import CrawlerInput from './Skills/Crawler/CrawlerInput';
 
+interface FileData {
+  name: string;
+  fileId?: string;
+  status?: 'uploading' | 'uploaded' | 'failed';
+}
 
 const ParentComponent = () => {
   const [knowledgeRetrieval, setKnowledgeRetrieval] = useState(true);
   const [webCrawler, setWebCrawler] = useState(false);
+  const [files, setFiles] = useState<FileData[]>([]); // Add this line
 
   return (
     <>
-    <Capabilities 
-      knowledgeRetrieval={knowledgeRetrieval} 
-      setKnowledgeRetrieval={setKnowledgeRetrieval}
-      webCrawler={webCrawler} 
-      setWebCrawler={setWebCrawler} 
-    />      
-    {knowledgeRetrieval && <UploadFiles_Configure />}
-    {webCrawler && <CrawlerInput />}
+      <Capabilities 
+        knowledgeRetrieval={knowledgeRetrieval} 
+        setKnowledgeRetrieval={setKnowledgeRetrieval}
+        webCrawler={webCrawler} 
+        setWebCrawler={setWebCrawler} 
+      />      
+      {knowledgeRetrieval && <UploadFiles_Configure files={files} setFiles={setFiles} />} {/* Pass the props here */}
+      {webCrawler && <CrawlerInput />}
     </>
   );
 };
